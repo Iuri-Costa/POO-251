9ca483b8aef1959d107f353764f9f4f8d48efba3
Iuri <iuri.aragao62@aluno.ifce.edu.br>
2025-05-17 15:24:30 -0300

    tirei comentários desnecessários  no MediaNotas.java

--- atividade-02/MediaNotas.java
+++ atividade-02/MediaNotas.java
@@ -4,25 +4,20 @@ public class MediaNotas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Pergunta ao usuário quantas notas deseja digitar
         System.out.print("Quantas notas você deseja digitar? ");
         int quantidade = scanner.nextInt();
 
-        // Cria um array para armazenar as notas
         double[] notas = new double[quantidade];
         double soma = 0;
 
-        // Lê as notas e armazena no array
         for (int i = 0; i < quantidade; i++) {
             System.out.print("Digite a nota " + (i + 1) + ": ");
             notas[i] = scanner.nextDouble();
             soma += notas[i];
         }
 
-        // Calcula a média
         double media = soma / quantidade;
 
-        // Exibe a média
         System.out.printf("A média das notas é: %.2f%n", media);
 
         scanner.close();
