f1b61a1748da326386f5179f7bbbaa52e00718b1
Iuri-Aragao <iuri.aragao62@aluno.ifce.edu.br>
2025-05-24 14:31:06 -0300

    comentários desnecessários retirados

--- atividade-04/Lâmpada/AtributosLampada.java
+++ atividade-04/Lâmpada/AtributosLampada.java
@@ -15,7 +15,7 @@ public class AtributosLampada {
         System.out.println("A lâmpada está desligada");
     }
 
-    void mostrarStatus() { // Corrigido de 'mostarStatus' para 'mostrarStatus'
+    void mostrarStatus() {
         String status = ligada ? "ligada" : "desligada";
         System.out.println("A lâmpada está " + status + ", da cor " + cor + " e de potência " + potencia); // Adicionado espaço antes de "potência"
     }
