80e969b084fccc304648e38d42359d7924c14e27
Iuri-Aragao <iuri.aragao62@aluno.ifce.edu.br>
2025-06-26 20:53:04 -0300

    jkh

--- atividade_08/reinandoPublic/Cavalo/Cavalo.java
+++
@@ -1,6 +0,0 @@
-package atividade_08.reinandoPublic.cavalo;
-public class Cavalo{
-    public double velocidade;
-    public String cor;
-    public String nome;
-}
--- atividade_08/reinandoPublic/Vaca/Vaca.java
+++
@@ -1,5 +0,0 @@
-public class Vaca {
-    public int peso;
-    public  String cor;
-    public String nome;
-}
