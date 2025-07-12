e02a1ffb5e66ad4edc6356e6339bbe0a52288221
Iuri-Aragao <iuri.aragao62@aluno.ifce.edu.br>
2025-07-12 18:22:41 -0300

    nao

--- atividade_10/instancia/Intanciar.java
+++ atividade_10/instancia/Intanciar.java
@@ -4,7 +4,7 @@ import atividade_10.Main.Professor;
 public class Intanciar {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno("Iuri", 17, "masculino", 272727, "TI");
-        Professor professor1 = new Professor("Rebeca", 40, "feminino", "POO", 12345);
+        Professor professor1 = new Professor("Rebeca", 30, "feminino", "POO", 12345);
 
         professor1.exibeInfo();
         aluno1.exibeInfo();
