f72412e5e898af4c02cc38c44cfcb41d73be7774
Iuri-Aragao <iuri.aragao62@aluno.ifce.edu.br>
2025-07-11 22:50:08 -0300

    consertando erro de digitação no pacote instancia

--- atividade_09/instancia/intanciar.java
+++ atividade_09/instancia/intanciar.java
@@ -1,7 +1,7 @@
 package instancia;
 import Main.Aluno;
 import Main.Professor;
-public class intanciar {
+public class Intanciar {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno("Iuri", 17, "masculino", 272727, "TI");
         Professor professor1 = new Professor("Rebeca", 40, "feminino", "POO", 12345);
