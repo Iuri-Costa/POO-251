f8e397211a6cfac655d974d57ea031bd7111759c
Iuri-Aragao <iuri.aragao62@aluno.ifce.edu.br>
2025-07-25 19:14:19 -0300

    m

--- atividade_11/TesteEmpresa.java
+++ atividade_11/TesteEmpresa.java
@@ -3,19 +3,17 @@ import empresa.Gerente;
 
 public class TesteEmpresa {
     public static void main(String[] args) {
-        // Instanciando Gerente
         Gerente gerente = new Gerente("Ana Souza", 40, 9500.00, "Financeiro");
-        gerente.exibirInformacoes(); // sobrescrito
-        gerente.planejar(); // sobrecarregado
+        gerente.exibirInformacoes();
+        gerente.planejar();
         gerente.planejar("Redução de Custos");
         gerente.planejar("Revisão Orçamentária", 15);
 
         System.out.println("------");
 
-        // Instanciando Desenvolvedor
         Desenvolvedor dev = new Desenvolvedor("Carlos Lima", 30, 8000.00, "Java");
-        dev.exibirInformacoes(); // sobrescrito
-        dev.programar(); // sobrecarregado
+        dev.exibirInformacoes();
+        dev.programar();
         dev.programar("Sistema de Login");
         dev.programar("Sistema de Vendas", true);
     }
